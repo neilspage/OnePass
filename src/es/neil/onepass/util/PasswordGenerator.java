e2cac8e6425ff6f665d43826dd7d0b4d3b52e79f
Neil <37527017+neilspage@users.noreply.github.com>
2018-12-16 00:25:34 +1100

    Fixed method name.

--- src/es/neil/onepass/util/PasswordGenerator.java
+++ src/es/neil/onepass/util/PasswordGenerator.java
@@ -11,10 +11,10 @@ import java.security.SecureRandom;
 public class PasswordGenerator {
     public static String computePass(String masterpass, String service) throws Exception {
         String concatArgs = masterpass+"!--+--!"+service; //concatonate the two arguments, and then MD5 hash them.
-        return new String(encryptRc4(service+service+service+service+service+service+service+service+service+service+service+service+service,concatArgs+concatArgs+concatArgs+concatArgs+concatArgs+concatArgs+concatArgs+concatArgs+concatArgs),"US-ASCII").replaceAll("\\W", "");
+        return new String(encryptAES(service+service+service+service+service+service+service+service+service+service+service+service+service,concatArgs+concatArgs+concatArgs+concatArgs+concatArgs+concatArgs+concatArgs+concatArgs+concatArgs),"US-ASCII").replaceAll("\\W", "");
     }
 
-    private static byte[] encryptRc4(String toEncrypt, String key) throws Exception{
+    private static byte[] encryptAES(String toEncrypt, String key) throws Exception{
         SecureRandom sr = new SecureRandom(key.getBytes());
         KeyGenerator kg = KeyGenerator.getInstance("AES");
         kg.init(sr);
